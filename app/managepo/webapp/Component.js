33ad22d34c55812d63af07686f3c0cd687b43ab0
Sindhu Mogare <sindhumogare5@gmail.com>
2025-05-15 04:13:30 +0000

    “cchanges in Components.js

--- app/managepo/webapp/Component.js
+++ app/managepo/webapp/Component.js
@@ -5,7 +5,7 @@ sap.ui.define(
 
         return Component.extend("bosch.po.managepo.Component", {
             metadata: {
-                manifest: "json"
+                manifest: "json" 
             }
         });
     }
